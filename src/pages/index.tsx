3cb6587b816f9eb9239695b90397735d8cac97ca
Lokochris <joseluismanco37@gmail.com>
2023-03-31 23:12:52 -0500

    splash

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Splash } from '@/components/Splash'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,10 +13,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div>
-          <h1 className='text-blue-400 text-5xl font-bold'>Hola mundo</h1>
-        </div>
+      <main className="h-screen">
+        <Splash/>
+        
       </main>
     </>
   )
